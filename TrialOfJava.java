0a44cbf359fa7c27c6988ec6e92da9ede8812ac7
DeepakRaval <ravaldeepak0696@gmail.com>
2021-05-09 13:07:16 +0530

    sorting of an elements

--- TrialOfJava.java
+++ TrialOfJava.java
@@ -1,45 +1,25 @@
 package tryExample.delete;
 
-public class TrialOfJava <T> {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TrialOfJava{
 	
-	T obj1;
-	T obj2;
-	
-	public TrialOfJava(T obj1, T obj2) {
-		super();
-		this.obj1 = obj1;
-		this.obj2 = obj2;
-	}
 
 	public static void main(String[] args) {
 		
-		TrialOfJava<Integer> toi = new TrialOfJava<Integer>(18, 90);
-		TrialOfJava<String> toi1 = new TrialOfJava<String>("A","A");
-		TrialOfJava<Double> toi2 = new TrialOfJava<Double>(1.8, 9.60);
-		
-		display(toi.obj1,toi.obj2);
-		display(toi1.obj1,toi1.obj2);
-		display(toi2.obj1,toi2.obj1);
-		
+		Integer [] num = {7,7,5,9,1,2,3,4,5,5,3,1,2,3,6};
+		SortingOfnum(num);
 		
 	}
 	
-	static <T> void display(T obj1,T obj2){
-		System.out.println("*************** Display Method ****************");
+	static void SortingOfnum(Integer [] numIn){
+	
+		List<Integer> numInt =  Arrays.asList(numIn);
+		Collections.sort(numInt);
+		System.out.println(numInt);
 		
-		if(obj1 instanceof Integer && obj2 instanceof Integer){
-			int num1 = (int)obj1;
-			int num2 = (int)obj2;
-			System.out.println("Addition = "+ Integer.sum(num1, num2));
-		}else if(obj1 instanceof Double && obj2 instanceof Double){
-			double num1 = (double)obj1;
-			double num2 = (double)obj2;
-			System.out.println("Additon of float = "+ (num1 + num2));
-		}else{
-			System.out.println("Please make sure your ^Datatype^");
-		}
-				
-		System.out.println("***********************************************");
 	}
 	
 	
